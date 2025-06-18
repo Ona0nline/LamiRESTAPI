a28eec8a3a661fab10fa5032c7f1f57baeb67e0c
Onalerona <onzwanejhb024@student.wethinkcode.co.za>
2025-06-18 09:15:20 +0200

    Disbaled Springsecurity for now, h2 database availiable fopr viewing.

--- src/main/java/com/onaonline/lami/lami_backend/security/SecurityConfig.java
+++ src/main/java/com/onaonline/lami/lami_backend/security/SecurityConfig.java
@@ -1,24 +1,24 @@
-package com.onaonline.lami.lami_backend.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.context.annotation.Bean;
-
-@Configuration
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        Cross site request forgery protection not neccessary because vele the api and client are on different sites and state changes will be made my json objects
-//         Public access to /home
-//.anyRequest().authenticated(): All other endpoints still require login if needed
-        return http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/home", "/lami", "/h2-console/**").permitAll()
-                        .anyRequest().permitAll()
-                )
-                .build();
-    }
-}
+//package com.onaonline.lami.lami_backend.security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.context.annotation.Bean;
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+////        Cross site request forgery protection not neccessary because vele the api and client are on different sites and state changes will be made my json objects
+////         Public access to /home
+////.anyRequest().authenticated(): All other endpoints still require login if needed
+//        return http
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/home", "/lami", "/h2-console/**").permitAll()
+//                        .anyRequest().permitAll()
+//                )
+//                .build();
+//    }
+//}
